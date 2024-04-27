ab6da82d6318b2362234bfc563170a670f009406
AJITH KUMAR K <106876826+ajithakdev@users.noreply.github.com>
2024-04-28 02:02:26 +0530

    Update predictive System.py

--- predictive System.py
+++ predictive System.py
@@ -43,7 +43,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 # loading the saved model
-loaded_model = pickle.load(open('C:/Users/Lenovo/Downloads/MainProject/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 # Define the input data
 input_data = np.array([197.07600,206.89600,192.05500,0.00289,0.00001,0.00166,0.00168,0.00498,0.01098,0.09700,0.00563,0.00680,0.00802,0.01689,0.00339,26.77500,0.422229,0.741367,-7.348300,0.177551,1.743867,0.085569])
