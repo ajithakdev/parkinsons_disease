fa112298a7ddb327f9b28f4e6bdd2c50708bbbde
AJITH KUMAR K <106876826+ajithakdev@users.noreply.github.com>
2024-04-28 15:11:21 +0530

    Update parkinson_s_disease_prediction.py

--- parkinson_s_disease_prediction.py
+++ parkinson_s_disease_prediction.py
@@ -40,22 +40,17 @@ print(X)
 
 print(Y)
 
-scaler = StandardScaler()
-# Fit the scaler on the training data before splitting
-#scaler = StandardScaler()
-#scaler.fit(X)  # Fit on the entire dataset before splitting
-
-scaler.fit(X_train)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
 
 print(X.shape, X_train.shape, X_test.shape)
 
-
+scaler = StandardScaler()
+scaler.fit(X_train) 
 
 X_train = scaler.transform(X_train)
-
 X_test = scaler.transform(X_test)
+
 
 print(X_train)
 
